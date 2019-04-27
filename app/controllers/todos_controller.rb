d78804b5fc7eb15be438e783580b003ad6c93588
Vladyslav Toltykh <daduskacpokus@gmail.com>
2019-04-27 12:23:57 +0300

    new

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,4 +8,18 @@ def show
 	@todo = Todo.find(params[:id])
 end
 
+def new
+	@todo = Todo.new
+end
+
+def create
+	@todo = Todo.create todo_params
+	redirect_to todo_path(@todo)
+end
+
+private
+		def todo_params
+			params.require(:todo).permit(:title, :description)
+		end
+
 end
